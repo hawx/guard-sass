08b5a42413f54b39d5b5e3cf58c188eef2cca54a
Joshua Hawxwell <m@hawx.me>
2011-10-01 11:25:21 +0100

    Specs massively improved

---
+++ spec/guard/sass/runner_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+
+describe Guard::Sass::Runner do
+
+  subject { Guard::Sass::Runner }
+  
+  let(:watcher)   { Guard::Watcher.new('^(.*)\.s[ac]ss$') }
+  let(:formatter) { subject.instance_variable_get(:@formatter) }
+  let(:defaults)  { Guard::Sass::DEFAULTS }
+  
+  before do
+    FileUtils.stub :mkdir_p
+    File.stub :open
+    IO.stub(:read).and_return('')
+  end
+  
+  
+  describe '#run' do
+  
+    it 'returns a list of changed files' do
+      subject.new([watcher], defaults).run(['a.sass'])[0].should == ['css/a.css']
+    end
+    
+    context 'if errors when compiling' do
+      subject { Guard::Sass::Runner.new([watcher], defaults) }
+      before { IO.stub(:read).and_return('body { color: red;') }
+      
+      it 'shows a warning message' do
+        formatter.should_receive(:error).with('Sass > Syntax error: Invalid CSS after "body ": expected selector, was "{ color: red;"
+        on line 1 of a.sass', :notification => 'rebuild of a.sass failed')
+        subject.run(['a.sass'])
+      end
+      
+      it 'returns false' do
+        subject.run(['a.sass'])[1].should == false
+      end
+      
+    end
+    
+    context 'if no errors when compiling' do
+      subject { Guard::Sass::Runner.new([watcher], defaults) }
+    
+      it 'shows a success message' do
+        formatter.should_receive(:success).with('-> compiled a.sass to css/a.css',
+          :notification => 'compiled a.sass to css/a.css')
+        subject.run(['a.sass'])
+      end
+      
+      it 'returns true' do
+        subject.run(['a.sass'])[1].should == true
+      end
+    end
+  
+    it 'compiles the files' do
+      a = Guard::Sass::DEFAULTS[:load_paths]
+      Guard::Sass::DEFAULTS[:load_paths] = ['sass']
+      
+      mock_engine = mock(::Sass::Engine)
+      ::Sass::Engine.should_receive(:new).with('', {
+        :syntax => :sass, :load_paths => ['sass'], 
+        :style => :nested, :debug_info => false
+      }).and_return(mock_engine)
+      mock_engine.should_receive(:render)
+      
+      subject.new([watcher], defaults).run(['a.sass'])
+
+      Guard::Sass::DEFAULTS[:load_paths] = a
+    end
+  
+  end
+
+end
--- spec/guard/sass_spec.rb
+++ spec/guard/sass_spec.rb
@@ -1,263 +1,128 @@
 require 'spec_helper'
 
 describe Guard::Sass do
+
   subject { Guard::Sass.new }
+  
+  let(:runner)    { Guard::Sass::Runner.new([]) }
 
-  describe "#initialize" do
-    it "should set default output path" do
-      subject.options[:output].should == 'css'
+  before do
+    subject.instance_variable_set :@runner, runner
+    runner.stub  :run
+  end
+  
+  describe '#initialize' do
+  
+    context 'when no options given' do
+      it 'uses defaults' do
+        subject.options.should == Guard::Sass::DEFAULTS
+      end
     end
-
-    it "should set default style" do
-      subject.options[:style].should == :nested
+    
+    context 'when options given' do
+      subject { Guard::Sass.new([], {:noop => true, :hide_success => true, :style => :compact}) }
+    
+      it 'merges them with defaults' do
+        subject.options.should == {
+          :output       => 'css',
+          :notification => true,
+          :shallow      => false,
+          :style        => :compact,
+          :debug_info   => false,
+          :noop         => true,
+          :hide_success => true,
+          :load_paths   => Dir.glob('**/**').find_all {|i| File.directory?(i) }
+        }
+      end
     end
-
-    it "should be able to set the style" do
-      gs = Guard::Sass.new(nil, {:style => 'compressed'})
-      gs.options[:style].should == 'compressed'
+    
+    context 'with an :input option' do
+      subject { Guard::Sass.new([], {:input => 'app/styles'}) }
+    
+      it 'creates a watcher' do
+        subject.should have(1).watchers
+      end
+      
+      it 'watches all *.s[ac]ss files' do
+        subject.watchers.first.pattern.should == %r{^app/styles/(.+\.s[ac]ss)$}
+      end
+      
+      context 'without an output option' do
+        it 'sets the output directory to the input directory' do
+          subject.options[:output].should == 'app/styles'
+        end
+      end
+      
+      context 'with an output option' do
+        subject { Guard::Sass.new([], {:input => 'app/styles', :output => 'public/styles'}) }
+      
+        it 'uses the output directory' do
+          subject.options[:output].should == 'public/styles'
+        end
+      end
+    end
+    
+  end
+  
+  describe '#ignored?' do
+    it 'is true if file begins with underscore' do
+      subject.ignored?('some/_file.sass').should be_true
+    end
+    
+    it 'is false if file does not begin with underscore' do
+      subject.ignored?('_some/file.sass').should be_false
     end
   end
 
-  describe "#build_sass" do
-    it "should convert sass to css" do
-      file = "sass-test/_sass/screen.sass"
-
-      res = <<EOS
-body {
-  color: red; }
-
-html {
-  color: blue; }
-
-.error, .badError {
-  border: 1px red;
-  background: #ffdddd; }
-
-.error.intrusion, .intrusion.badError {
-  font-size: 1.3em;
-  font-weight: bold; }
-
-.badError {
-  border-width: 3px; }
-EOS
-
-      subject.build_sass(file).should == res
+  describe '#run_all' do
+    subject { Guard::Sass.new([Guard::Watcher.new('(.*)\.s[ac]ss')]) }
+  
+    before do 
+      Dir.stub(:glob).and_return ['a.sass', 'b.scss', 'c.ccss', 'd.css', 'e.scsc']
+      subject.stub :notify
     end
-
-    it "should convert scss to css" do
-      file = "sass-test/_sass/print.scss"
-
-      res = <<EOS
-.error, .badError {
-  border: 1px #f00;
-  background: #fdd; }
-
-.error.intrusion, .intrusion.badError {
-  font-size: 1.3em;
-  font-weight: bold; }
-
-.badError {
-  border-width: 3px; }
-EOS
-
-      subject.build_sass(file).should == res
-    end
-
-    it "should add debugging information" do
-      file = "sass-test/_sass/print.scss"
-
-      res = <<'EOS'
-@media -sass-debug-info{filename{font-family:}line{font-family:\000031}}
-.error, .badError {
-  border: 1px #f00;
-  background: #fdd; }
-
-@media -sass-debug-info{filename{font-family:}line{font-family:\000035}}
-.error.intrusion, .intrusion.badError {
-  font-size: 1.3em;
-  font-weight: bold; }
-
-@media -sass-debug-info{filename{font-family:}line{font-family:\0000310}}
-.badError {
-  border-width: 3px; }
-EOS
-
-      Guard::Sass.new([], {:debug_info => true}).build_sass(file).should == res
-    end
-  end
-
-  describe "#get_output" do
-    before do
-      m = mock("listener")
-      m.stub!(:directory).and_return("sass-test")
-      ::Guard.stub(:listener).and_return(m)
-    end
-
-    it "should change extension to css" do
-      subject.options[:output] = "css"
-      r = subject.get_output("sass-test/_sass/screen.sass")
-      r[-3..-1].should == "css"
-    end
-
-    it "should change the folder to /css (by default)" do
-      subject.options[:output] = "css"
-      r = subject.get_output("sass-test/_sass/screen.sass")
-      File.dirname(r).should == "sass-test/css"
-    end
-
-    it "should not change the file name" do
-      subject.options[:output] = "csS"
-      r = subject.get_output("sass-test/_sass/screen.sass")
-      File.basename(r)[0..-5].should == "screen"
-    end
-  end
-
-  describe "#ignored?" do
-    it "is true if file begins with _" do
-      subject.ignored?("some/dir/_file.sass").should be_true
-    end
-
-    it "is false if file does not begin with _" do
-      subject.ignored?("some/dir/file.sass").should be_false
-    end
-  end
-
-  describe "#run_all" do
-    it "should rebuild all files being watched" do
-      Guard::Sass.stub(:run_on_change).with([]).and_return([])
-      Guard.stub(:guards).and_return([subject])
+  
+    it 'calls #run_on_change with all watched files' do
+      subject.should_receive(:run_on_change).with(['a.sass', 'b.scss'])
       subject.run_all
     end
   end
-
-  describe "#run_on_change" do
-    before do
-      subject.stub!(:notify)
-      m = mock("listener")
-      m.stub!(:directory).and_return("sass-test")
-      ::Guard.stub(:listener).and_return(m)
+  
+  describe '#run_on_change' do
+    subject { Guard::Sass.new([Guard::Watcher.new('(.*)\.s[ac]ss')]) }
+    
+    before { subject.stub :notify }
+  
+    context 'if paths given contain partials' do
+      it 'calls #run_all' do
+        subject.should_receive(:run_all)
+        subject.run_on_change(['sass/_partial.sass'])
+      end
     end
-
-    it "calls #run_all if partials changed" do
-      subject.should_receive(:run_all).and_return(nil)
-      subject.run_on_change(["some/_partial.sass"])
+    
+    it 'starts the Runner' do
+      runner.should_receive(:run).with(['a.sass'])
+      subject.run_on_change(['a.sass'])
     end
-
-    it "builds the sass files" do
-      subject.should_receive(:build_sass).and_return("text")
-      File.any_instance.should_receive(:write).with("text")
-      subject.run_on_change(["some/file.sass"])
+    
+    it 'notifies the other guards about changed files' do
+      runner.stub(:run).and_return([['a.css', 'b.css'], true])
+      subject.should_receive(:notify).with(['a.css', 'b.css'])
+      subject.run_on_change(['a.sass', 'b.scss'])
     end
-
-    it "displays warning if sass syntax error raised" do
-      subject.should_receive(:build_sass).and_raise(::Sass::SyntaxError.new('hio'))
-      ::Guard::UI.should_receive(:error)
-      ::Guard::Notifier.should_receive(:notify)
-      subject.run_on_change(["some/bad_file.sass"])
+  end
+  
+  describe '#notify' do
+    it 'notifies other guards' do
+      dummy_guard = mock(Guard::Guard)
+      ::Guard.stub(:guards).and_return([dummy_guard, subject])
+      
+      Guard::Watcher.stub(:match_files).with(dummy_guard, ['a.css']).and_return(['a.css'])
+      subject.should_not_receive(:run_on_change)
+      dummy_guard.should_receive(:run_on_change).with(['a.css'])
+      
+      subject.notify(['a.css'])
     end
   end
 
-  describe "#notify" do
-    it "should notify other guards upon completion" do
-      other_guard = mock('guard')
-      other_guard.should_receive(:watchers).and_return([])
-      Guard.stub(:guards).and_return([subject, other_guard])
-      subject.notify([])
-    end
-  end
-
-  describe "#styling" do
-    it "should be able to change the style" do
-      subject.options[:style] = :compressed
-
-      subject.options[:style].should == :compressed
-    end
-
-    it "should be nested by default" do
-      file = "sass-test/_sass/screen.sass"
-
-      res = <<-EOS
-body {
-  color: red; }
-
-html {
-  color: blue; }
-
-.error, .badError {
-  border: 1px red;
-  background: #ffdddd; }
-
-.error.intrusion, .intrusion.badError {
-  font-size: 1.3em;
-  font-weight: bold; }
-
-.badError {
-  border-width: 3px; }
-EOS
-
-      subject.build_sass(file).should == res
-    end
-
-    it "should accept compact" do
-      subject.options[:style] = :compact
-
-      file = "sass-test/_sass/screen.sass"
-
-      res = <<-EOS
-body { color: red; }
-
-html { color: blue; }
-
-.error, .badError { border: 1px red; background: #ffdddd; }
-
-.error.intrusion, .intrusion.badError { font-size: 1.3em; font-weight: bold; }
-
-.badError { border-width: 3px; }
-EOS
-
-      subject.build_sass(file).should == res
-    end
-
-    it "should accept compressed" do
-      subject.options[:style] = :compressed
-
-      file = "sass-test/_sass/screen.sass"
-
-      res = "body{color:red}html{color:blue}.error,.badError{border:1px red;background:#fdd}.error.intrusion,.intrusion.badError{font-size:1.3em;font-weight:bold}.badError{border-width:3px}\n"
-
-      subject.build_sass(file).should == res
-    end
-
-    it "should accept expanded" do
-      subject.options[:style] = :expanded
-
-      file = "sass-test/_sass/screen.sass"
-
-      res = <<-EOS
-body {
-  color: red;
-}
-
-html {
-  color: blue;
-}
-
-.error, .badError {
-  border: 1px red;
-  background: #ffdddd;
-}
-
-.error.intrusion, .intrusion.badError {
-  font-size: 1.3em;
-  font-weight: bold;
-}
-
-.badError {
-  border-width: 3px;
-}
-EOS
-
-      subject.build_sass(file).should == res
-    end
-  end
 end
