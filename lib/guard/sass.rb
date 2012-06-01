58f2bd6007df72beda6b2376d4548fec5c069e98
Joshua Hawxwell <m@hawx.me>
2012-06-01 10:21:20 +0100

    Gets correct paths, and throws correct error on fail.

--- lib/guard/sass.rb
+++ lib/guard/sass.rb
@@ -5,7 +5,7 @@ require 'sass/plugin'
 
 module Guard
   class Sass < Guard
-  
+
     autoload :Runner,    'guard/sass/runner'
     autoload :Formatter, 'guard/sass/formatter'
 
@@ -25,11 +25,11 @@ module Guard
     # @param options [Hash]
     # @option options [String] :input
     #   The input directory
-    # @option options [String] :output 
+    # @option options [String] :output
     #   The output directory
     # @option options [Array<String>] :load_paths
-    #   List of directories you can @import from 
-    # @option options [Boolean] :shallow 
+    #   List of directories you can @import from
+    # @option options [Boolean] :shallow
     #   Whether to output nested directories
     # @option options [Boolean] :debug_info
     #   Whether to output file and line number info for FireSass
@@ -44,52 +44,41 @@ module Guard
         options[:output] = options[:input] unless options.has_key?(:output)
         watchers << ::Guard::Watcher.new(%r{^#{ options.delete(:input) }/(.+\.s[ac]ss)$})
       end
-      
+
       options = DEFAULTS.merge(options)
       @runner = Runner.new(watchers, options)
       super(watchers, options)
     end
-    
-    def partial?(path)
-      File.basename(path)[0,1] == "_"
-    end
 
     # If option set to run all on start, run all when started.
+    #
+    # @raise [:task_has_failed]
     def start
-      if options[:all_on_start]
-        run_all
-      end
+      run_all if options[:all_on_start]
     end
 
     # Build all files being watched
     #
-    # @return [Boolean] No errors?
+    # @raise [:task_has_failed]
     def run_all
-      run_on_change(
-        Watcher.match_files(
-          self, 
-          Dir.glob(File.join(::Guard.listener.directory, '**', '*.s[ac]ss')).
-            map {|f| f[::Guard.listener.directory.size+1..-1] }.
-            reject {|f| partial?(f) }
-        )
-      )
+      files = Dir.glob('**/*.s[ac]ss').reject {|f| partial?(f) }
+      run_on_change Watcher.match_files(self, files)
     end
-    
+
     # Build the files given. If a 'partial' file is found (begins with '_') calls
     # {#run_all} as we don't know which other files need to use it.
-    # 
+    #
     # @param paths [Array<String>]
-    # @return [Boolean] No errors?
+    # @raise [:task_has_failed]
     def run_on_change(paths)
-      has_partials = paths.any? {|f| partial?(f) }
-      return run_all if has_partials
-      
+      return run_all if paths.any? {|f| partial?(f) }
+
       changed_files, success = @runner.run(paths)
-      
       notify changed_files
-      success
+
+      throw :task_has_failed unless success
     end
-    
+
     # Notify other guards about files that have been changed so that other guards can
     # work on the changed files.
     #
@@ -99,6 +88,10 @@ module Guard
         paths = Watcher.match_files(guard, changed_files)
         guard.run_on_change(paths) unless paths.empty?
       end
+    end
+
+    def partial?(path)
+      File.basename(path)[0,1] == "_"
     end
 
   end
--- lib/guard/sass/runner.rb
+++ lib/guard/sass/runner.rb
@@ -2,11 +2,11 @@ require 'sass'
 
 module Guard
   class Sass
-    
+
     class Runner
-    
+
       attr_reader :options
-    
+
       # @param watchers [Array<Guard::Watcher>]
       # @param options [Hash] See Guard::Sass::DEFAULTS for available options
       def initialize(watchers, options={})
@@ -14,14 +14,14 @@ module Guard
         @options   = options
         @formatter = Formatter.new(:hide_success => options[:hide_success])
       end
-    
+
       # @param files [Array<String>]
       # @return [Array<Array,Boolean>]
       def run(files)
         changed_files, errors =  compile_files(files)
         [changed_files, errors.empty?]
       end
-      
+
       private
 
       # @param files [Array<String>] Files to compile
@@ -30,45 +30,45 @@ module Guard
       def compile_files(files)
         errors        = []
         changed_files = []
-        
+
         # Assume partials have been checked for previously, so no partials are included here
-        files.each do |file|          
+        files.each do |file|
           begin
             css_file = write_file(compile(file), get_output_dir(file), file)
             message = options[:noop] ? "verified #{file}" : "compiled #{file} to #{css_file}"
             @formatter.success "-> #{message}", :notification => message
             changed_files << css_file
-            
+
           rescue ::Sass::SyntaxError => e
             message = (options[:noop] ? 'validation' : 'rebuild') + " of #{file} failed"
             errors << message
             @formatter.error "Sass > #{e.sass_backtrace_str(file)}", :notification => message
           end
         end
-        
+
         [changed_files.compact, errors]
       end
-      
+
       # @param file [String] Path to sass/scss file to compile
       # @return [String] Compiled css.
       def compile(file)
-        content = IO.read(File.join(::Guard.listener.directory, file))
-        
+        content = IO.read(file)
+
         sass_options = {
           :syntax     => file[-4..-1].to_sym,
           :load_paths => options[:load_paths],
           :style      => options[:style],
           :debug_info => options[:debug_info]
         }
-        
+
         ::Sass::Engine.new(content, sass_options).render
       end
-      
+
       # @param file [String]
       # @return [String] Directory to write +file+ to
       def get_output_dir(file)
         folder = options[:output]
-  
+
         unless options[:shallow]
           @watchers.product([file]).each do |watcher, file|
             if matches = file.match(watcher.pattern)
@@ -79,10 +79,10 @@ module Guard
             end
           end
         end
-        
+
         folder
       end
-      
+
       # Write file contents, creating directories where required.
       #
       # @param content [String] Contents of the file
@@ -96,10 +96,10 @@ module Guard
           FileUtils.mkdir_p(dir)
           File.open(path, 'w') {|f| f.write(content) }
         end
-        
+
         path
       end
-    
+
     end
   end
 end
